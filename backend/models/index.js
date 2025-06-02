4cf9afc92740bd02391638fc421672aae8335460
GOH01 <wave4446@naver.com>
2025-06-02 21:13:56 +0900

    delete code

--- backend/models/index.js
+++ backend/models/index.js
@@ -36,37 +36,6 @@ async function createTable(tableName){
   return Model;
 }
 
-// async function createDynamicTable(profile, tableName) {
-//   const DynamicModel = await createTable(tableName);
-
-//   let core_row = -1;
-//   for (let row = 0; row < profile.length; row++) {
-//     if (core_row === -1) {
-//       core_row = row;
-//       continue;
-//     }
-//     if (profile[row].length === 1) {
-//       core_row = -1;
-//       continue;
-//     }
-
-//     for (let column = 1; column < profile[row].length; column++) {
-//       const taskName = profile[core_row][column - 1];
-//       const usage = profile[row][column];
-
-//       //빈 값 또는 null/undefined 방어 처리
-//       if (!taskName || usage === undefined || usage === null) {
-//         continue;
-//       }
-//       await DynamicModel.create({
-//         task: profile[core_row][column - 1],
-//         core: profile[row][0],
-//         usaged: profile[row][column],
-//       });
-//     }
-//   }
-//   console.log("현재처리중:",profile[row])
-// }
 
 async function createDynamicTable(profile, tableName) {
   const DynamicModel = await createTable(tableName);
